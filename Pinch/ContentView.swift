aacf475459e30a08a1035154494203560054c8fb
Jersy Fernandes <102835855+JersyFernandesJF@users.noreply.github.com>
2022-07-13 22:32:03 +0100

    fix: finall

--- Pinch/ContentView.swift
+++ Pinch/ContentView.swift
@@ -13,7 +13,9 @@ struct ContentView: View {
     @State private var imageScale: CGFloat = 1
     @State private var imageOffset: CGSize = .zero
     @State private var isDrawerOpen: Bool = false
-  
+    
+    let pages: [Page] = pagesData
+    @State private var pageIndex: Int = 1
     
     func resetImageState() {
         return withAnimation(.spring()) {
@@ -21,12 +23,16 @@ struct ContentView: View {
             imageOffset = .zero
         }
     }
+    
+    func currentPage() -> String {
+        return pages[pageIndex - 1].imageName
+    }
     var body: some View {
         NavigationView {
             ZStack{
                 Color.clear
                 
-                Image("magazine-front-cover")
+                Image(currentPage())
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .cornerRadius(10)
@@ -148,6 +154,21 @@ struct ContentView: View {
                                 isDrawerOpen.toggle()
                             }
                         })
+                    ForEach(pages) {
+                        item in
+                        Image(item.thumbnailName)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 80)
+                            .cornerRadius(8)
+                            .shadow(radius: 4)
+                            .opacity(isDrawerOpen ? 1 : 0)
+                            .animation(.easeOut(duration: 0.5), value: isDrawerOpen )
+                            .onTapGesture(perform: {
+                                isAnimating = true
+                                pageIndex = item.id
+                            })
+                    }
                     
                     Spacer()
                 }
---
+++ Pinch/Data/PageData.swift
@@ -0,0 +1,13 @@
+//
+//  PageData.swift
+//  Pinch
+//
+//  Created by Jersy Fernandes on 13/07/2022.
+//
+
+import Foundation
+
+let pagesData: [Page] = [
+    Page(id: 1, imageName: "magazine-front-cover"),
+    Page(id: 2, imageName: "magazine-back-cover")
+]
